3579811b1d5a44883238a863c877196dc9525e76
super <super3codes@gmail.com>
2023-10-10 21:09:56 +0200

    first commit

---
+++ src/lib/todoService.js
@@ -0,0 +1,39 @@
+const baseUrl = process.env.REACT_APP_API_PATH
+console.log(baseUrl)
+
+export const loadTodos = () => {
+  return fetch(baseUrl)
+    .then(res => res.json())
+}
+
+export const createTodo = (todo) => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(todo)
+  }).then(res => res.json())
+}
+
+export const saveTodo = (todo) => {
+  return fetch(`${baseUrl}/${todo.id}`, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(todo)
+  }).then(res => res.json())
+}
+
+export const destroyTodo = (id) => {
+  return fetch(`${baseUrl}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+}
